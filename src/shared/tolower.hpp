768d979a8533d85711ac721af38ab1bdbbd6db87
Philipp M. Wittershagen <philippwittershagen@web.de>
2017-10-08 22:40:10 +0200

    locale-tolower hotfix

--- src/shared/tolower.hpp
+++ src/shared/tolower.hpp
@@ -6,14 +6,13 @@
 
 namespace pw
 {
-	std::locale const utf8("en_US.UTF-8");
-
 	std::string tolower(const std::string &_string)
 	{
+		std::locale const locale;
 		std::wstring result = pw::stringconvert::widen(_string);
 		for (typename std::wstring::size_type i = 0; i < result.length(); ++i)
 		{
-			result[i] = std::tolower(result[i], utf8);
+			result[i] = std::tolower(result[i], locale);
 		}
 		return pw::stringconvert::narrow(result);
 	}
